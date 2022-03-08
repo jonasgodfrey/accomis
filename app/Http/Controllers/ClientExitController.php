b056c78fb5d089bf2bf4e017f94c611a8b25cc41
Godfrey Jonas <41231839+jonasgodfrey@users.noreply.github.com>
2022-03-08 13:55:37 +0100

    Completed ACCOMIS Update

--- app/Http/Controllers/ClientExitController.php
+++ app/Http/Controllers/ClientExitController.php
@@ -39,8 +39,7 @@ class ClientExitController extends Controller
 
     public function client_exit()
     {
-
-        $collection = '';
+        
 
         // $responsex = Http::withBasicAuth('acomin', 'itsupport@acomin.org')->get('https://kobo.humanitarianresponse.info/assets/aweG5RNYvHNj7TMM2xHvxL/submissions/?format=json');
 
@@ -48,77 +47,77 @@ class ClientExitController extends Controller
 
         // return $kobos;
 
-        // $collection = Http::withBasicAuth('acomin', 'itsupport@acomin.org')->get('https://kobo.humanitarianresponse.info/assets/acM6WkAQpDKeMpvVx7uDSe/submissions/?format=json');
+        $collection = Http::withBasicAuth('acomin', 'itsupport@acomin.org')->get('https://kobo.humanitarianresponse.info/assets/acM6WkAQpDKeMpvVx7uDSe/submissions/?format=json');
+      
+            $collection = json_decode($collection->getBody(true)->getContents());
+            
+            
+    
+            foreach ($collection as $key => $row){
+                // dd($collection);
+               
+                $record  = Cei::insertOrIgnore([
+                   "recordid" => $row->_id,                
+                   "start" => $row->start,
+                   "end" => $row->end,
+                   "today" => $row->today,
+                   "state" => $row->state,
+                   "lga" => $row->lga,
+                   "cbo" => $row->cbo,
+                   "cboemail"=> $row->cboemail,
+                   "ward" =>isset($row->ward)? $row->ward:'',
+                   "hf" => $row->hf,
+                   "qtr" => $row->qtr,
+                   "resp_name" => isset($row->resp_name)? $row->resp_name:'',
+                   "child_name" => isset($row->child_name)? $row->child_name:'',
+                   "resp_cat" => isset($row->resp_cat)? $row->resp_cat:'',
+                   "address" => isset($row->address)? $row->address:'',
+                   "phone" => isset($row->phone)? $row->phone:'',
+                   "occupation" => isset($row->occupation)? $row->occupation:'',
+                   "other_occupation" => isset($row->other_occupation)? $row->other_occupation:'',
+                   "resp_edu" => isset($row->resp_edu)? $row->resp_edu:'',
+                   "created_at" => isset($row->created_at)? $row->created_at:'',
+                   "updated_at" => isset($row->updated_at)? $row->updated_at:'',
+                   "other_edu" => isset($row->other_edu)? $row->other_edu:'',
+                   "service_cat" => isset($row->service_cat)? $row->service_cat:'',
+                   "serv_received" => isset($row->serv_received)? $row->serv_received:'',
+                   "other_received" => isset($row->other_received)? $row->other_received:'',
+                   "freq_visit" => isset($row->freq_visit)? $row->freq_visit:'',
+                   "llin_recipient" => isset($row->llin_recipient)? $row->llin_recipient:'',
+                   "llin_where" => isset($row->llin_where)? $row->llin_where:'',
+                   "where_others" => isset($row->where_others)? $row->where_others:'',
+                   "llin_where" => isset($row->llin_where)? $row->llin_where:'',
+                   "llin_freq" => isset($row->llin_freq)? $row->llin_freq:'',
+                   "llin_freq" => isset($row->llin_freq)? $row->llin_freq:'',
+                   "llin_sleep" => isset($row->llin_sleep)? $row->llin_sleep:'',
+                   "sleep_often" => isset($row->sleep_often)? $row->sleep_often:'',
+                   "why_no" => isset($row->why_no)? $row->why_no:'',
+                   "ipt_recipient" => isset($row->ipt_recipient)? $row->ipt_recipient:'',
+                   "freq_ipt" => isset($row->freq_ipt)? $row->freq_ipt:'',
+                   "sp_recipient" => isset($row->sp_recipient)? $row->sp_recipient:'',
+                   "given_sp" => isset($row->given_sp)? $row->given_sp:'',
+                   "sp_no" => isset($row->sp_no)? $row->sp_no:'',
+                   "why_others" => isset($row->why_others)? $row->why_others:'',
+                   "smc_recipient" => isset($row->smc_recipient)? $row->smc_recipient:'',
+                   "smc_age" => isset($row->smc_age)? $row->smc_age:'',
+                   "malaria" => isset($row->malaria)? $row->malaria:'',
+                   "tested_when" => isset($row->tested_when)? $row->tested_when:'',
+                   "act_recipient" => isset($row->act_recipient)? $row->act_recipient:'',
+                   "drug_received" => isset($row->drug_received)? $row->drug_received:'',
+                   "act_finish" => isset($row->act_finish)? $row->act_finish:'',
+                   "drug_finish_no" => isset($row->drug_finish_no)? $row->drug_finish_no:'',
+                   "rating" => isset($row->rating)? $row->rating:'',
+                   "dissatisfied" => isset($row->dissatisfied)? $row->dissatisfied:'',
+                   "others" => isset($row->others)? $row->others:'',
+                   "satisfied" => isset($row->satisfied)? $row->satisfied:'',
+                   "suggestion" => isset($row->suggestion)? $row->suggestion:'',
+                   "upload_image" => isset($row->upload_image)? $row->upload_image:'',
+                   "store_gps" => isset($row->store_gps)? $row->store_gps:''
+                         
+           ]);
+        }
 
-        //     $collection = json_decode($collection->getBody(true)->getContents());
-
-
-
-        //     foreach ($collection as $key => $row){
-        //         // dd($collection);
-
-        //         $record  = Cei::insertOrIgnore([
-        //            "recordid" => $row->_id,
-        //            "start" => $row->start,
-        //            "end" => $row->end,
-        //            "today" => $row->today,
-        //            "state" => $row->state,
-        //            "lga" => $row->lga,
-        //            "cbo" => $row->cbo,
-        //            "cboemail"=> $row->cboemail,
-        //            "ward" =>isset($row->ward)? $row->ward:'',
-        //            "hf" => $row->hf,
-        //            "qtr" => $row->qtr,
-        //            "resp_name" => isset($row->resp_name)? $row->resp_name:'',
-        //            "child_name" => isset($row->child_name)? $row->child_name:'',
-        //            "resp_cat" => isset($row->resp_cat)? $row->resp_cat:'',
-        //            "address" => isset($row->address)? $row->address:'',
-        //            "phone" => isset($row->phone)? $row->phone:'',
-        //            "occupation" => isset($row->occupation)? $row->occupation:'',
-        //            "other_occupation" => isset($row->other_occupation)? $row->other_occupation:'',
-        //            "resp_edu" => isset($row->resp_edu)? $row->resp_edu:'',
-        //            "created_at" => isset($row->created_at)? $row->created_at:'',
-        //            "updated_at" => isset($row->updated_at)? $row->updated_at:'',
-        //            "other_edu" => isset($row->other_edu)? $row->other_edu:'',
-        //            "service_cat" => isset($row->service_cat)? $row->service_cat:'',
-        //            "serv_received" => isset($row->serv_received)? $row->serv_received:'',
-        //            "other_received" => isset($row->other_received)? $row->other_received:'',
-        //            "freq_visit" => isset($row->freq_visit)? $row->freq_visit:'',
-        //            "llin_recipient" => isset($row->llin_recipient)? $row->llin_recipient:'',
-        //            "llin_where" => isset($row->llin_where)? $row->llin_where:'',
-        //            "where_others" => isset($row->where_others)? $row->where_others:'',
-        //            "llin_where" => isset($row->llin_where)? $row->llin_where:'',
-        //            "llin_freq" => isset($row->llin_freq)? $row->llin_freq:'',
-        //            "llin_freq" => isset($row->llin_freq)? $row->llin_freq:'',
-        //            "llin_sleep" => isset($row->llin_sleep)? $row->llin_sleep:'',
-        //            "sleep_often" => isset($row->sleep_often)? $row->sleep_often:'',
-        //            "why_no" => isset($row->why_no)? $row->why_no:'',
-        //            "ipt_recipient" => isset($row->ipt_recipient)? $row->ipt_recipient:'',
-        //            "freq_ipt" => isset($row->freq_ipt)? $row->freq_ipt:'',
-        //            "sp_recipient" => isset($row->sp_recipient)? $row->sp_recipient:'',
-        //            "given_sp" => isset($row->given_sp)? $row->given_sp:'',
-        //            "sp_no" => isset($row->sp_no)? $row->sp_no:'',
-        //            "why_others" => isset($row->why_others)? $row->why_others:'',
-        //            "smc_recipient" => isset($row->smc_recipient)? $row->smc_recipient:'',
-        //            "smc_age" => isset($row->smc_age)? $row->smc_age:'',
-        //            "malaria" => isset($row->malaria)? $row->malaria:'',
-        //            "tested_when" => isset($row->tested_when)? $row->tested_when:'',
-        //            "act_recipient" => isset($row->act_recipient)? $row->act_recipient:'',
-        //            "drug_received" => isset($row->drug_received)? $row->drug_received:'',
-        //            "act_finish" => isset($row->act_finish)? $row->act_finish:'',
-        //            "drug_finish_no" => isset($row->drug_finish_no)? $row->drug_finish_no:'',
-        //            "rating" => isset($row->rating)? $row->rating:'',
-        //            "dissatisfied" => isset($row->dissatisfied)? $row->dissatisfied:'',
-        //            "others" => isset($row->others)? $row->others:'',
-        //            "satisfied" => isset($row->satisfied)? $row->satisfied:'',
-        //            "suggestion" => isset($row->suggestion)? $row->suggestion:'',
-        //            "upload_image" => isset($row->upload_image)? $row->upload_image:'',
-        //            "store_gps" => isset($row->store_gps)? $row->store_gps:'',
-
-        //    ]);
-        // }
-
-
+        
         $page_views = request()->page_views;
 
         // dd($page_views??100);
@@ -145,6 +144,7 @@ class ClientExitController extends Controller
             // $clients = ClientExitQuestionare::all()->sortDesc();
             $clients = ClientExitQuestionare::latest()->paginate($page_views);
             $kobocei = Cei::latest();
+           
         }
         if ($role == "Me") {
             // $clients = ClientExitQuestionare::all()->sortDesc();
@@ -161,17 +161,9 @@ class ClientExitController extends Controller
 
         return view('backend.clientexit.clientexit')->with([
             'clients' => $clients,
-            'health_facilities' => $health_facilities,
+            'health_facilities'=> $health_facilities,
             'collection' => $collection,
             'kobocei' => $kobocei
-        ]);
-    }
-
-    public function view_more($id)
-    {
-        $client = ClientExitQuestionare::where('id', $id)->get();
-        return view('backend.clientexit.view_more')->with([
-            'clientexit' => $client,
         ]);
     }
 
@@ -245,7 +237,7 @@ class ClientExitController extends Controller
             'service_satisfaction_aid' => $request->customer_help,
             'facility_improvment_suggestion' => $request->customer_help_improve,
             'auth_user_email' => $user->email,
-            'cbo_name' => $cbo_name,
+            'cbo_name'=> $cbo_name,
             'spo' => $spo_email,
             'state' => $state,
             'month' => $month,
@@ -255,35 +247,23 @@ class ClientExitController extends Controller
             'attachment' => $filename,
         ]);
     }
-
-    public function multiple_delete(Request $request)
-    {
-        $ids = $request->ids;
-        $files = ClientExitQuestionare::whereIn('id', $ids)->get();
-
-        foreach ($files as $record) {
-
-            if (Storage::delete("public/attachments/" . $record->attachment)) {
-                $id = $record->id;
-                ClientExitQuestionare::where('id', $id)->delete();
-            } 
-        }
-
-        return response('Records deleted successfully 😔');
-    }
-    
     public function delete($id)
     {
         $files = ClientExitQuestionare::where('id', $id)->get();
 
         foreach ($files as $file) {
-            if (Storage::delete("public/attachments/" . $file->attachment)) {
+            if (Storage::delete("public/attachments/".$file->attachment)) {
                 ClientExitQuestionare::where('id', $id)->delete();
                 Session::flash('flash_message', 'Client Exit Report Deleted successfully');
-            } else {
+                return redirect()->back();
+            }else{
                 Session::flash('error_message', 'Sorry an error occured, try again later !');
+                return redirect()->back();
             }
-            return redirect()->back();
         }
+
     }
+
+   
 }
+ 
