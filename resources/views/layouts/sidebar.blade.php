0b7837b04501a087d6d0567fe3d7d885b58ad721
Godfrey Jonas <41231839+jonasgodfrey@users.noreply.github.com>
2022-04-22 12:10:37 +0100

    Update sidebar.blade.php
    adding cei to m and e

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -76,7 +76,7 @@
                      </li>
                  @endcan
 
-                 @can('admin_spo')
+                 @can('admin_me')
                      <li class="nav-item">
                          <a href="{{ route('cei_analysis.barchart') }}" class="nav-link active">
                              <i class="nav-icon  fa fa-chart-bar"></i>
@@ -110,7 +110,7 @@
                      </li>
                  @endcan
 
-                 @can('admin_spo')
+                 @can('admin_me')
                      <li class="nav-item">
                          <a href="{{ route('cei_analysis') }}" class="nav-link">
                              <i class="nav-icon  fa fa-book"></i>
